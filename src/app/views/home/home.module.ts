6422564db723503e972a5b6083fe5b25a59dea70
Samuel Zapote Granjeno <samuel@zapote.org>
2020-09-19 03:15:46 -0500

    feat(Home): Added home content placeholder

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-
+import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
+    MatCardModule,
     SharedModule,
   ]
 })
