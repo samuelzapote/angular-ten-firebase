2712f1a7a7dd3f2d27f8830eefd87e2cf69b9261
Samuel Zapote Granjeno <samuel@zapote.org>
2020-09-19 00:56:16 -0500

    feat(Routing): Added paths to feature modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,29 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 import { canActivate, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+import { Routes, RouterModule } from '@angular/router';
 
-const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['']);
 const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['auth']);
 
 const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule),
+    ...canActivate(redirectUnauthorizedToLogin),
+  },
   {
     path: 'auth',
     loadChildren: () => import('./views/auth/auth.module').then(m => m.AuthModule),
     ...canActivate(redirectLoggedInToHome),
   },
   {
-    path: '',
-    loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule),
+    path: 'profile',
+    loadChildren: () => import('./views/profile/profile.module').then(m => m.ProfileModule),
+    ...canActivate(redirectUnauthorizedToLogin),
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./views/account/account.module').then(m => m.AccountModule),
     ...canActivate(redirectUnauthorizedToLogin),
   },
 ];
