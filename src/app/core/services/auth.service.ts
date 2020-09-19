5533f82cd263df47b450825f38a0059236b62103
Samuel Zapote Granjeno <samuel@zapote.org>
2020-09-18 19:23:15 -0500

    feat(auth): Added auth logic to service

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,16 +1,59 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { Observable } from 'rxjs';
-import { User } from 'firebase';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { UserCredential } from '@firebase/auth-types'
+
+import { LoadingService } from './loading.service';
+import { NewUser } from 'src/app/views/auth/models/new-user.model';
+import { User } from 'src/app/shared/models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  public user: Observable<User> = this.auth.user;
+  private userAuth$: Observable<User>;
+  private user$: Observable<User>;
 
-  constructor(private auth: AngularFireAuth) { }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private loadingService: LoadingService,
+  ) {
+    this.userAuth$ = this.afAuth.user;
+    this.user$ = this.afAuth.authState.pipe(
+      switchMap(user => user ? this.afs.doc<User>(`users/${user.uid}`).valueChanges() : of(null))
+    );
+  }
 
-  public login(): void { }
-  public logout(): void { }
+  get getUser(): Observable<User> {
+    return this.user$;
+  }
+
+  get getUserAuth(): Observable<User> {
+    return this.userAuth$;
+  }
+
+  public login(email: string, password: string): Promise<UserCredential> {
+    this.loadingService.toggleLoading(true);
+    return this.afAuth.signInWithEmailAndPassword(email, password);
+  }
+
+  public logout(): Promise<void> {
+    this.loadingService.toggleLoading(true);
+    return this.afAuth.signOut();
+  }
+
+  public register({email, password, username, firstName, lastName, displayName}: NewUser): Promise<void | User> {
+    return this.afAuth.createUserWithEmailAndPassword(email, password)
+      .then((credentials) => {
+        const newUser: User = {
+          email: credentials.user.email,
+          uid: credentials.user.uid,
+          displayName: displayName,
+        };
+        this.afs.collection<User>('users').add(newUser);
+      });
+  }
 }
