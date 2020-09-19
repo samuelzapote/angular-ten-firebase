58602e29c230029b07a7f1ddb099da5346309053
Samuel Zapote Granjeno <samuel@zapote.org>
2020-09-19 00:52:54 -0500

    fix(Sidenav): Added missing logic for sidenav

--- src/app/core/components/sidenav/sidenav.component.ts
+++ src/app/core/components/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
+  @Output() logout: EventEmitter<void> = new EventEmitter();
+  @Output() navigatedOut: EventEmitter<void> = new EventEmitter();
+
+  public sideNavButtons = [
+    { label: 'home', path: '', icon: 'home' },
+    { label: 'profile', path: 'profile', icon: 'account_circle' },
+    { label: 'account', path: 'account', icon: 'settings' },
+  ];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  public onLogout(): void {
+    this.logout.emit();
   }
 
+  public onNavigateOut(): void {
+    this.navigatedOut.emit();
+  }
 }
