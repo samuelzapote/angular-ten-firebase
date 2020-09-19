ab7cdfa8455634c10ad144229309d4e4c97f9b89
Samuel Zapote Granjeno <samuel@zapote.org>
2020-09-19 00:55:26 -0500

    feat(Layout): Auth Logic and Sidenav Toggling

--- src/app/core/components/layout/layout.component.ts
+++ src/app/core/components/layout/layout.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 import { Observable } from 'rxjs';
-import { User } from 'firebase';
+import { Router } from '@angular/router';
 
 import { AuthService } from '../../services/auth.service';
+import { LoadingService } from '../../services/loading.service';
+import { User } from 'src/app/shared/models/user.model';
 
 @Component({
   selector: 'app-layout',
@@ -10,11 +13,23 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
-  public user: Observable<User> = this.authService.user;
+  @ViewChild('sidenav') sidenav: MatSidenav;
 
-  constructor(private authService: AuthService) { }
+  public userAuth: Observable<User> = this.authService.getUserAuth;
+
+  constructor(private authService: AuthService, private loadingService: LoadingService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  public async onLogout(): Promise<void> {
+    await this.authService.logout();
+    this.sidenav.close();
+    this.loadingService.toggleLoading(false);
+    this.router.navigate(['auth']);
+  }
+
+  public onNavigatedOut(): void {
+    this.sidenav.close();
+  }
 }
