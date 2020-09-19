072229a633d6ba889ae1ad03f468d57ef43eb9a3
Samuel Zapote Granjeno <samuel@zapote.org>
2020-09-19 00:45:18 -0500

    feat(AuthComponent): Added connection to auth micro-service

--- src/app/views/auth/auth.component.ts
+++ src/app/views/auth/auth.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TitleCasePipe } from '@angular/common';
 
+import { AuthService } from 'src/app/core/services/auth.service';
+import { LoadingService } from 'src/app/core/services/loading.service';
 import { AuthField } from './models/auth-field.model';
 import { AuthFormFieldsService } from './services/auth-form-fields.service';
 import { AuthFormState } from './types/auth-form-state.type';
@@ -8,28 +12,55 @@ import { AuthFormState } from './types/auth-form-state.type';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
+  providers: [TitleCasePipe],
 })
 export class AuthComponent implements OnInit {
   public formState: AuthFormState;
   public formFields: AuthField[];
   public authForm: FormGroup;
 
-  constructor(private authFieldsService: AuthFormFieldsService) {
+  get inLoginMode() { return this.formState === 'login' };
+  get inRegisterMode() { return this.formState === 'register' };
+
+  constructor(
+    private authFormFieldsService: AuthFormFieldsService,
+    private authService: AuthService,
+    private loadingService: LoadingService,
+    private router: Router,
+    private titleCasePipe: TitleCasePipe,
+  ) {
     this.formState  = 'login';
-    this.formFields = this.authFieldsService.getFormFields(this.formState);
+    this.formFields = this.authFormFieldsService.getFormFields(this.formState);
     this.authForm = this.getAuthFormGroup(this.formFields);
   }
 
   public ngOnInit(): void { }
 
-  public onSubmit(form: FormGroup): void {
+  public async onSubmit(form: FormGroup): Promise<void> {
+    const { email, password, username, firstName, lastName } = form.value;
+    if (this.passwordConfirmed(form.value.password, form.value.passwordConfirm)) {
+      await this.authService.register({
+        email,
+        password,
+        username,
+        firstName,
+        lastName,
+        displayName: this.titleCasePipe.transform(`${firstName} ${lastName}`),
+      });
+    }
+    await this.authService.login(form.value.email, form.value.password);
+    this.loadingService.toggleLoading(false);
+    this.router.navigate(['']);
+  }
 
+  private passwordConfirmed(password: string, passwordConfirm: string): boolean {
+    return this.inRegisterMode && password === passwordConfirm;
   }
 
   public onToggleFormState(currentState: AuthFormState): void {
     this.formState = currentState === 'login' ? 'register' : 'login';
-    this.formFields = this.authFieldsService.getFormFields(this.formState);
+    this.formFields = this.authFormFieldsService.getFormFields(this.formState);
     this.authForm = this.getAuthFormGroup(this.formFields);
   }
 
@@ -42,10 +73,15 @@ export class AuthComponent implements OnInit {
   }
 
   public getErrorMessage(control: FormControl): string {
-    return control.hasError('required') ? 'You must enter a value' : '';
+    if (control.hasError('required')) { return 'You must enter a value' }
+    if (control.hasError('email')) { return 'You must enter a valid email' }
+    return '';
   }
 
-  public getAlternateOption(state: AuthFormState): AuthFormState {
-    return state === 'login' ? 'register' : 'login';
+  public getAlternateOption(state: AuthFormState): {button: AuthFormState, text: string} {
+    return {
+      button: this.inLoginMode ? `register` : 'login',
+      text: this.inLoginMode ? `Don't have an account?` : 'Already have an accout?',
+    }
   }
 }
